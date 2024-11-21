6336f051360326058ccad6f4a0eee2f150c9c318
Pablo Souza <150870639+PSdevj@users.noreply.github.com>
2024-11-21 11:21:21 -0300

    Level Desing

--- Invasão da SunnyLand/Assets/Scripts/ControllPlayer.cs
+++ Invasão da SunnyLand/Assets/Scripts/ControllPlayer.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class ControllPlayer : MonoBehaviour
 {
+
+    public Rigidbody2D corpoPlayer;
+    public float velocidadePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        corpoPlayer = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movimentação();
     }
+
+
+    public void Movimentação()
+    {
+        velocidadePlayer = Input.GetAxis("Horizontal") * 3.5f;
+        corpoPlayer.velocity = new Vector2(velocidadePlayer, corpoPlayer.velocity.y);
+    }
+    
 }
