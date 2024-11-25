0535ff624c501e879e021535aa1d5f67bd8d2d99
Pablo Souza <150870639+PSdevj@users.noreply.github.com>
2024-11-25 10:10:15 -0300

    Movimento Dos Inimigos

---
+++ Invasão da SunnyLand/Assets/Scripts/MoveInimigo.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInimigo : MonoBehaviour
+{
+
+    private bool moveInimigo = false;
+
+
+    public float velocidadeInimigo = 3f;
+    public Transform pontoA;
+    public Transform pontoB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x > pontoA.position.x)
+        {
+            moveInimigo = true;
+            transform.localScale = new Vector3(1, 1, 1);
+
+        }
+        else if (this.transform.position.x < pontoB.position.x)
+        {
+            moveInimigo = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        }
+
+        if (moveInimigo)
+        {
+            transform.position = new Vector2(transform.position.x - velocidadeInimigo * Time.deltaTime, transform.position.y);
+        }
+        else if (!moveInimigo)
+        {
+            transform.position = new Vector2(transform.position.x + velocidadeInimigo * Time.deltaTime, transform.position.y);
+        }
+
+       
+        }
+    }
