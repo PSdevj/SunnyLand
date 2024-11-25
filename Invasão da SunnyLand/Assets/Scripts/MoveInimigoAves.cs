0c4ed73b7a653778ca845d7fe6a86eb06a6d86f7
Pablo Souza <150870639+PSdevj@users.noreply.github.com>
2024-11-25 10:28:33 -0300

    Adicionando os Inimigos- Final

--- Invasão da SunnyLand/Assets/Scripts/MoveInimigoAves.cs
+++ Invasão da SunnyLand/Assets/Scripts/MoveInimigoAves.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class MoveInimigoAves : MonoBehaviour
 {
+
+    private bool moveInimigo = false;
+
+    public float velocidadeInimigo = 3f;
+    public Transform pontoA;
+    public Transform pontoB;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,28 @@ public class MoveInimigoAves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.transform.position.y > pontoA.position.y)
+        {
+            moveInimigo = true;
+            
+
+        }
+        else if (this.transform.position.y < pontoB.position.y)
+        {
+            moveInimigo = false;
+
+        }
+
+        if (moveInimigo)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - velocidadeInimigo * Time.deltaTime);
+        }
+        else if (!moveInimigo)
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + velocidadeInimigo * Time.deltaTime);
+        }
+
+
     }
 }
+
